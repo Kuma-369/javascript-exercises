0149d7dbf6a5333bf2e5b0dec3bdf11eaa4c28ff
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-11 15:32:45 +0100

    Complete repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,35 @@
-const repeatString = function() {
+const repeatString = function joinStringNum(string, num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR";
+    } else { 
+        for (let i = 0; i < num; i++) {
+            result += string;
+        }
 
+    }
+
+    return result;
 };
+
+console.log(repeatString("hey", 5));
+
+
+// function testing(computerInput, userInput) {
+//     let result = "";
+//     if (userInput < 0){
+//         return "ERROR";
+//     }
+//     else {
+//         for (let i = 0; i < userInput; i++) {
+//             result += computerInput;
+//         }
+//     }
+//     return result;
+// }
+
+
+// console.log(testing("ha",0))
 
 // Do not edit below this line
 module.exports = repeatString;
