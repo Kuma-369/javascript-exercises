07a53a6e6db068c03a6f832494dbc3d46ca163b9
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-11 17:01:59 +0100

    Complete reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function seperateCharacters(string) {
+    let newString = "";
+   
+    const arr = Array.from(string);
+    // return arr;
 
+    for (let i = arr.length - 1 ; i >= 0 ; i--) {
+        newString += arr[i];
+    }
+
+    return newString;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
