0cdc9bce7f98600a798cb905d8aeabd4cd59a2d2
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-14 15:08:45 +0100

    Complete tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheitTemp) {
+  return Math.round((fahrenheitTemp - 32) * 5 / 9 * 10) / 10;
+}; // (F - 32) * 5/9 
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(celsiusTemp) {
+  return Math.round((celsiusTemp * 9 / 5) + 32 * 10) / 10;
+}; // (C * 9/5) + 32
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
