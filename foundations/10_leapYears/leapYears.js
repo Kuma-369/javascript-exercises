2f3a8d573c70b4dd1afd4237e06bc7fd00ecafde
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-14 14:50:09 +0100

    Complete leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // year % 4 === 0 AND year % 100 !== 0;
+    // OR year % 400 === 0;
 
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(1800));
 
 // Do not edit below this line
 module.exports = leapYears;
