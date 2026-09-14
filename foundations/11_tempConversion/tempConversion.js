cf86d1020b6950c18556d9765c8a0478fdc4fe92
Kuma-369 <191901457+Kuma-369@users.noreply.github.com>
2026-09-14 15:14:03 +0100

    Update comments

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(fahrenheitTemp) {
   return Math.round((fahrenheitTemp - 32) * 5 / 9 * 10) / 10;
-}; // (F - 32) * 5/9 
+}; // (F - 32) * 5/9 , round up to 2 dp
 
 const convertToFahrenheit = function(celsiusTemp) {
   return Math.round((celsiusTemp * 9 / 5) + 32 * 10) / 10;
-}; // (C * 9/5) + 32
+}; // (C * 9/5) + 32, round up to 2 dp
 
 console.log(convertToCelsius(32));
 console.log(convertToFahrenheit(0));
